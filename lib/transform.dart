5fbf23a428d59ff3964eed09876f5231c999b292
mirajago <mirajago@gmail.com>
2021-06-16 11:59:53 +0200

    transform route adjusted

--- lib/transform.dart
+++ lib/transform.dart
@@ -7,7 +7,6 @@ class TransformRoute extends StatefulWidget {
 }
 
 class _TransformRouteState extends State<TransformRoute> {
-  double _rotationFactor = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,28 +18,12 @@ class _TransformRouteState extends State<TransformRoute> {
       body: Column(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
         Padding(
           padding: const EdgeInsets.only(top: 70.0, bottom: 30.0),
-          child: Transform(
-            transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.001)
-              ..rotateY(pi * _rotationFactor),
-            origin: Offset(MediaQuery.of(context).size.width / 4, 0),
             child: Image.asset(
               'assets/pics/cuckoo.png',
               width: MediaQuery.of(context).size.width / 2,
               fit: BoxFit.cover,
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 20.0, bottom: 200),
-          child: Slider(
-              onChanged: (double value) {
-                setState(() {
-                  _rotationFactor = value;
-                });
-              },
-              value: _rotationFactor),
-        ),
       ]),
     );
   }
